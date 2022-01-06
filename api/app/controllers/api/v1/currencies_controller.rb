9667709f27219071f6dcffb6cc1fee23b02bb142
Gilbert <gilwong3105@gmail.com>
2022-01-05 23:55:35 -0800

    Add method to retrieve coin logo in get call

--- api/app/controllers/api/v1/currencies_controller.rb
+++ api/app/controllers/api/v1/currencies_controller.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'json'
 
 class Api::V1::CurrenciesController < ApplicationController
   before_action :set_currency, only: %i[show update destroy]
@@ -7,10 +8,17 @@ class Api::V1::CurrenciesController < ApplicationController
   def index
     page = !params[:page].nil? ? params[:page].to_i : 1
     limit = !params[:limit].nil? ? params[:limit].to_i : 10
-    uri = URI("#{ENV['COIN_MARKET_URL']}/listings/latest?start=#{page}&limit=#{limit}")
-    res = Net::HTTP.get_response(uri, { 'X-CMC_PRO_API_KEY' => (ENV['COIN_MARKET_API_KEY']).to_s })
+    uri = URI("#{coin_market_endpoint}/listings/latest?start=#{page}&limit=#{limit}")
+    res = Net::HTTP.get_response(uri, { 'X-CMC_PRO_API_KEY' => coin_market_key })
+    payload = JSON.parse(res.body).deep_symbolize_keys
+    coins = payload[:data]
 
-    render json: res.body
+    # gets coin logo from meta route
+    coins.map do |coin|
+      coin[:logo] = get_crypto_logo(coin[:id])
+    end
+
+    render json: coins
   end
 
   # GET /currencies/1
@@ -55,9 +63,23 @@ class Api::V1::CurrenciesController < ApplicationController
     params.require(:currency).permit(:coin_name, :value, :page, :limit)
   end
 
-  def get_crypto_meta(id)
-    uri = URI("#{ENV['COIN_MARKET_URL']}/info?id=#{id}")
-    res = Net::HTTP.get_response(uri, { 'X-CMC_PRO_API_KEY' => (ENV['COIN_MARKET_API_KEY']).to_s })
-    res.body
+  def coin_market_endpoint
+    (ENV['COIN_MARKET_URL']).to_s
+  end
+
+  def parsed_coin_market_response(res)
+    payload = JSON.parse(res.body).deep_symbolize_keys
+    payload[:data]
+  end
+
+  def coin_market_key
+    (ENV['COIN_MARKET_API_KEY']).to_s
+  end
+
+  def get_crypto_logo(id)
+    res = Net::HTTP.get_response(URI("#{coin_market_endpoint}/info?id=#{id}"),
+                                 { 'X-CMC_PRO_API_KEY' => coin_market_key })
+    data = parsed_coin_market_response(res)
+    data[id.to_s.to_sym][:logo]
   end
 end
