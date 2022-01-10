e053f3d1519eae7575bce270c31833203afe100d
Gilbert <gilwong3105@gmail.com>
2022-01-09 23:53:29 -0800

    Start crypto detail screen

---
+++ ui/src/screens/CryptoDetails/CryptoDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+interface CryptoDetailsProps {}
+
+const CryptoDetails: React.FunctionComponent<CryptoDetailsProps> = props => {
+  return <View></View>;
+};
+
+export default CryptoDetails;
---
+++ ui/src/screens/CryptoDetails/index.ts
@@ -0,0 +1,1 @@
+export { default as CryptoDetails } from './CryptoDetails';
