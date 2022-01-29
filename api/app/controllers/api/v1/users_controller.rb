f2df25f6b681550c325458f7795e8b672d8d752d
Gilbert <gilwong3105@gmail.com>
2022-01-29 01:58:05 -0800

    Hash password with bcrypt

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -16,9 +16,10 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
+		@user.password = params[:password]
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -38,6 +39,10 @@ class Api::V1::UsersController < ApplicationController
     @user.destroy
   end
 
+	def current_user
+		User.find_by_email!(params[:email])
+	end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
@@ -47,6 +52,6 @@ class Api::V1::UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :first_name, :last_name, :password)
   end
 end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,16 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+	has_secure_password
+	validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+	def password
+    @password
+  end
+
+  def password=(new_password)
+		@password = new_password
+    self.password_digest = BCrypt::Password.create(new_password)
+  end
 end
